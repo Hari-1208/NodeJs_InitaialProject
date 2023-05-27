ee38cfd1399acae4989c958748f247f627212c4e
Hari-1208 <hariprasathime@gmail.com>
2023-05-27 19:48:09 +0530

    changed the controllers to class components instead of functional

--- src/controller/addressController.js
+++ src/controller/addressController.js
@@ -4,95 +4,93 @@ const models = require("../dataBase/models");
 const addressSchema = models.address;
 const userSchema = models.user;
 
-const getAddressById = async (req, res) => {
-  try {
-    //finding particular data in db using id
-    const address = await addressSchema.findById(req.params.id);
-    if (!address)
-      return res.status(404).send({
+class addressController {
+  static getAddressById = async (req, res) => {
+    try {
+      //finding particular data in db using id
+      const address = await addressSchema.findById(req.params.id);
+      if (!address)
+        return res.status(404).send({
+          status: 404,
+          message: "Address with given ID is not fuond!",
+        });
+      return res.status(200).send({
+        status: 200,
+        message: "Data fetched succesfully",
+        data: address,
+      });
+    } catch (err) {
+      res.status(404).send({
         status: 404,
         message: "Address with given ID is not fuond!",
+        data: err,
       });
-    return res.status(200).send({
-      status: 200,
-      message: "Data fetched succesfully",
-      data: address,
-    });
-  } catch (err) {
-    res.status(404).send({
-      status: 404,
-      message: "Address with given ID is not fuond!",
-      data: err,
-    });
-  }
-};
+    }
+  };
 
-const addNewAddress = async (req, res) => {
-  try {
-    const reqBody = req.body;
+  static addNewAddress = async (req, res) => {
+    try {
+      const reqBody = req.body;
 
-    //check user is in user Db
-    const user = await userSchema.findById(reqBody.userId);
-    if (!user)
-      return res.status(404).send({
-        status: 404,
-        message: "user does not exist!",
+      //check user is in user Db
+      const user = await userSchema.findById(reqBody.userId);
+      if (!user)
+        return res.status(404).send({
+          status: 404,
+          message: "user does not exist!",
+        });
+
+      const newAddress = {
+        userId: reqBody.userId,
+        address: reqBody.address,
+      };
+
+      const addedAddress = await addressSchema.create(newAddress);
+      return res.status(200).send({
+        status: 200,
+        message: `Successfully address added for ${user.name}`,
+        data: addedAddress,
       });
-
-    const newAddress = {
-      userId: reqBody.userId,
-      address: reqBody.address,
-    };
-
-    const addedAddress = await addressSchema.create(newAddress);
-    return res.status(200).send({
-      status: 200,
-      message: `Successfully address added for ${user.name}`,
-      data: addedAddress,
-    });
-  } catch (err) {
-    res.status(400).send({
-      status: 400,
-      message: "Bad Request",
-      data: err.message,
-    });
-  }
-};
-
-const getAddressListByUserId = async (req, res) => {
-  try {
-    //check user is in user Db
-    const user = await userSchema.findById(req.params.id);
-    if (!user)
-      return res.status(404).send({
-        status: 404,
-        message: "user does not exist!",
+    } catch (err) {
+      res.status(400).send({
+        status: 400,
+        message: "Bad Request",
+        data: err.message,
       });
+    }
+  };
 
-    //finding address list for given userId in address db using id
-    const addressList = await addressSchema.find({ userId: req.params.id });
-    if (!Boolean(addressList) || addressList.length == 0)
-      return res.status(404).send({
+  static getAddressListByUserId = async (req, res) => {
+    try {
+      //check user is in user Db
+      const user = await userSchema.findById(req.params.id);
+      if (!user)
+        return res.status(404).send({
+          status: 404,
+          message: "user does not exist!",
+        });
+
+      //finding address list for given userId in address db using id
+      const addressList = await addressSchema.find({ userId: req.params.id });
+      if (!Boolean(addressList) || addressList.length == 0)
+        return res.status(404).send({
+          status: 404,
+          message: "Address List for given userID is not fuond!",
+        });
+
+      return res.status(200).send({
+        status: 200,
+        message: `Succesfully fetched Address List of ${user.name}`,
+        data: addressList,
+      });
+    } catch (err) {
+      res.status(404).send({
         status: 404,
         message: "Address List for given userID is not fuond!",
+        data: err,
       });
+    }
+  };
+}
 
-    return res.status(200).send({
-      status: 200,
-      message: `Succesfully fetched Address List of ${user.name}`,
-      data: addressList,
-    });
-  } catch (err) {
-    res.status(404).send({
-      status: 404,
-      message: "Address List for given userID is not fuond!",
-      data: err,
-    });
-  }
-};
-
-module.exports = {
-  getAddressById,
-  addNewAddress,
-  getAddressListByUserId,
-};
+module.exports = addressController;
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,162 +3,157 @@ const models = require("../dataBase/models");
 //Getting schema collection
 const userSchema = models.user;
 
-const initialPage = async (req, res) => {
-  res.send("Basic Curd Operation in Nodejs");
-};
+class userController {
+  static initialPage = async (req, res) => {
+    res.send("Basic Curd Operation in Nodejs");
+  };
 
-const getAllUsers = async (req, res) => {
-  try {
-    const response = await userSchema.find();
-    res.status(200).send({
-      status: 200,
-      message: "Datas Listed succesfully",
-      data: response,
-    });
-  } catch (err) {
-    res.status(404).send({
-      status: 404,
-      message: "Error",
-      data: err,
-    });
-  }
-};
-
-const getUserById = async (req, res) => {
-  try {
-    //finding particular data in db using id
-    const item = await userSchema.findById(req.query.id);
-    if (!item?.name)
-      return res.status(404).send({
+  static getAllUsers = async (req, res) => {
+    try {
+      const response = await userSchema.find();
+      res.status(200).send({
+        status: 200,
+        message: "Datas Listed succesfully",
+        data: response,
+      });
+    } catch (err) {
+      res.status(404).send({
         status: 404,
-        message: "User with given ID is not fuond!",
+        message: "Error",
+        data: err,
       });
-    return res
-      .status(200)
-      .send({ status: 200, message: "Data fetched succesfully", data: item });
-  } catch (err) {
-    res.status(404).send({
-      status: 404,
-      message: "User with given ID is not fuond!",
-      data: err,
-    });
-  }
-};
+    }
+  };
 
-const addNewUser = async (req, res) => {
-  const newUser = new userSchema({
-    name: req.body.name,
-    mobileNumber: req.body.mobileNumber,
-    dob: req.body.mobileNumber,
-    emailId: req.body.emailId,
-  });
-
-  try {
-    const dbData = await userSchema.find();
-
-    let filteredItem = dbData.find((item) => {
-      if (
-        parseInt(item.mobileNumber) === parseInt(req.body.mobileNumber) ||
-        item.emailId === req.body.emailId
-      ) {
-        return item;
-      }
-    });
-    if (Boolean(filteredItem))
-      return res.status(201).send({
-        status: 201,
-        message: "User already exist!",
-      });
-    const addedUser = await newUser.save();
-    res.status(200).send({
-      status: 200,
-      message: "User added succesfully",
-      data: addedUser,
-    });
-  } catch (err) {
-    res.status(400).send({
-      status: 400,
-      message: "Bad Request",
-      data: err.message,
-    });
-  }
-};
-
-const updateUser = async (req, res) => {
-  try {
-    // look for the item if its not retuen 404 object not fount
-    const item = await userSchema.findById(req.params.id);
-    if (!item)
-      return res.status(404).send({
-        status: 404,
-        message: "User with given ID is not fuond!",
-      });
-
-    // update item
-    userSchema
-      .findByIdAndUpdate(req.params.id, req.body)
-      .then((response) => {
-        res.status(200).send({
-          status: 200,
-          message: "User updated succesfully",
+  static getUserById = async (req, res) => {
+    try {
+      //finding particular data in db using id
+      const item = await userSchema.findById(req.query.id);
+      if (!item?.name)
+        return res.status(404).send({
+          status: 404,
+          message: "User with given ID is not fuond!",
         });
-      })
-      .catch((err) => {
-        if (err) {
-          res.status(400).send({
-            message: "error",
-            data: err,
-          });
+      return res
+        .status(200)
+        .send({ status: 200, message: "Data fetched succesfully", data: item });
+    } catch (err) {
+      res.status(404).send({
+        status: 404,
+        message: "User with given ID is not fuond!",
+        data: err,
+      });
+    }
+  };
+
+  static addNewUser = async (req, res) => {
+    const newUser = new userSchema({
+      name: req.body.name,
+      mobileNumber: req.body.mobileNumber,
+      dob: req.body.mobileNumber,
+      emailId: req.body.emailId,
+    });
+
+    try {
+      const dbData = await userSchema.find();
+
+      let filteredItem = dbData.find((item) => {
+        if (
+          parseInt(item.mobileNumber) === parseInt(req.body.mobileNumber) ||
+          item.emailId === req.body.emailId
+        ) {
+          return item;
         }
       });
-  } catch (err) {
-    res.status(400).send({
-      message: "error",
-      data: err.message,
-    });
-  }
-};
-
-const deleteUser = async (req, res) => {
-  try {
-    // look for the item if its not retuen 404 object not fount
-    const item = await userSchema.findById(req.params.id);
-    if (!item)
-      return res.status(404).send({
-        status: 404,
-        message: "User with given ID is not fuond!",
-      });
-
-    //delete item
-    userSchema
-      .findByIdAndDelete(req.params.id)
-      .then((response) => {
-        res.status(200).send({
-          status: 200,
-          message: "User deleted succesfully",
+      if (Boolean(filteredItem))
+        return res.status(201).send({
+          status: 201,
+          message: "User already exist!",
         });
-      })
-      .catch((err) => {
-        if (err) {
-          res.status(400).send({
-            message: "error",
-            data: err,
-          });
-        }
+      const addedUser = await newUser.save();
+      res.status(200).send({
+        status: 200,
+        message: "User added succesfully",
+        data: addedUser,
       });
-  } catch (err) {
-    res.status(400).send({
-      message: "error",
-      data: err.message,
-    });
-  }
-};
+    } catch (err) {
+      res.status(400).send({
+        status: 400,
+        message: "Bad Request",
+        data: err.message,
+      });
+    }
+  };
 
-module.exports = {
-  initialPage,
-  getAllUsers,
-  getUserById,
-  addNewUser,
-  updateUser,
-  deleteUser,
-};
+  static updateUser = async (req, res) => {
+    try {
+      // look for the item if its not retuen 404 object not fount
+      const item = await userSchema.findById(req.params.id);
+      if (!item)
+        return res.status(404).send({
+          status: 404,
+          message: "User with given ID is not fuond!",
+        });
+
+      // update item
+      userSchema
+        .findByIdAndUpdate(req.params.id, req.body)
+        .then((response) => {
+          res.status(200).send({
+            status: 200,
+            message: "User updated succesfully",
+          });
+        })
+        .catch((err) => {
+          if (err) {
+            res.status(400).send({
+              message: "error",
+              data: err,
+            });
+          }
+        });
+    } catch (err) {
+      res.status(400).send({
+        message: "error",
+        data: err.message,
+      });
+    }
+  };
+
+  static deleteUser = async (req, res) => {
+    try {
+      // look for the item if its not retuen 404 object not fount
+      const item = await userSchema.findById(req.params.id);
+      if (!item)
+        return res.status(404).send({
+          status: 404,
+          message: "User with given ID is not fuond!",
+        });
+
+      //delete item
+      userSchema
+        .findByIdAndDelete(req.params.id)
+        .then((response) => {
+          res.status(200).send({
+            status: 200,
+            message: "User deleted succesfully",
+          });
+        })
+        .catch((err) => {
+          if (err) {
+            res.status(400).send({
+              message: "error",
+              data: err,
+            });
+          }
+        });
+    } catch (err) {
+      res.status(400).send({
+        message: "error",
+        data: err.message,
+      });
+    }
+  };
+}
+
+module.exports = userController;
